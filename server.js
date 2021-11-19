e91d0bfd7d21fed01508f494a5847d0dd654d0a2
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-19 13:05:59 -0800

    Add heroku, craete server and functions to handle request

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const {animals} = require('./data/animals');
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-});
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if(req.query){
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
 });
 
 function filterByQuery(query, animalsArray) {
@@ -54,3 +46,25 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
     
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result){
+      res.json(result);
+    } else {
+        res.send(404);
+    }
+  });
+
